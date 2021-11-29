00aa91f353a7b4aabba7e6f6e250ba9ffe45be6c
skyler-ferrante <sjf5462@rit.edu>
2021-11-29 18:20:33 -0500

    Added commands to get list of logins/bad logins
    Uses last and lastb commands

--- oneliners.sh
+++ oneliners.sh
@@ -10,3 +10,8 @@ cat log.pcap | awk '{print $3 "\n" $5}' | tr -d ':' | awk -F\. '$NF<49152' | sor
 # This gets pids from netstat, then runs ps -f pids
 # In this case we grep for http, to get the full process information of all processes communicating over http/https 
 netstat -tap | grep "ESTABLISHED" | grep "http" | awk -F/ '{print $(NF-1)}' | awk '{print $NF}' | xargs ps -f
+
+# Get list of all IP addresses with logins/bad logins (with count)
+# (For login, will not show for user named 'system boot')
+lastb --time-format 'notime' | awk '{print $(NF-1)}' | sort | uniq -c | sort -n
+last --time-format 'notime' | grep -v "system boot" | awk '{print $(NF-1)}' | sort | uniq -c | sort -n
