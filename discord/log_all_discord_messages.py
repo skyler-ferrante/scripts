87b871b942b887be85594cd5e8050aff06d67703
skyler-ferrante <sjf5462@rit.edu>
2021-11-29 17:44:05 -0500

    Adding discord logger and created discord folder

---
+++ discord/log_all_discord_messages.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+"""log_all_discord_messages.py:
+    Simple python discord client to log all discord messages
+    Especially useful for watching discord C2 traffic
+"""
+
+__author__ = "Skyler Ferrante"
+
+import discord
+import datetime
+import sys
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+# Ask for discord token (from stderr, that way we can still redirect stdout)
+TOKEN = input("Token: ").strip()
+print("Joining with token:",TOKEN, file=sys.stderr)
+
+class ReplyClient(discord.Client):
+        async def on_message(self, message):
+            print(message.content)
+            print(message.author)
+            print(datetime.datetime.now())
+
+client = ReplyClient()
+client.run(TOKEN)
